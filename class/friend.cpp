bf0f38c205eb23f912e2c387072a9bcd20529dca
1103617438@qq.com <1103617438@qq.com>
2026-05-08 10:41:10 +0800

    feat(class): 添加友元函数和运算符重载示例代码
    - 实现了 Box 类的基本结构和私有成员变量 width
    - 添加了友元函数 printWidth 用于访问私有成员变量
    - 实现了 ostream 输出流运算符重载友元函数
    - 添加了 + 运算符重载成员函数用于宽度相加操作
    - 包含了 explicit 构造函数防止隐式类型转换
    - 提供了完整的 main 函数演示友元函数和运算符重载功能

--- class/friend.cpp
+++ class/friend.cpp
@@ -2,21 +2,39 @@
 // Created by Administrator on 2026/5/8.
 //
 #include <iostream>
+#include <ostream>
 
 using namespace std;
 
 class Box
 {
+
+
     double width;
 public:
     friend void printWidth( Box box );
+    // 授权：允许这个全局函数访问我的私有变量
+    friend ostream& operator<<(ostream& os, const Box& box);
+    // 声明 + 运算符重载友元函数
+    Box operator+(const Box& b1);
     void setWidth( double wid );
+
+    Box() = default;
+
+
+    explicit Box(int i);
+
 };
+
 
 // 成员函数定义
 void Box::setWidth( double wid )
 {
     width = wid;
+}
+
+Box::Box(int i) {
+    setWidth(i);
 }
 
 // 请注意：printWidth() 不是任何类的成员函数
@@ -26,6 +44,23 @@ void printWidth( Box box )
     cout << "Width of box : " << box.width <<endl;
 }
 
+// 第一个参数是左边的 cout (ostream&)
+// 第二个参数是右边的 myBox (const Box&)
+ostream& operator<<(ostream& os, const Box& box) {
+    os << "Width: " << box.width; // ⚠️ 注意：width 是私有的！
+    return os;
+}
+
+// 重载 + 运算符：将两个 Box 的宽度相加
+Box Box:: operator+(const Box& other)
+{
+    Box result;
+    result.width = this->width + other.width;
+    return result;
+}
+void doSomething(Box b) {
+    // 处理盒子
+}
 // 程序的主函数
 int main( )
 {
@@ -35,6 +70,11 @@ int main( )
 
     // 使用友元函数输出宽度
     printWidth( box );
+    std::cout << box;
 
+    Box a(1), b(2);
+    Box c = a + b;
+    cout << c;
+    // doSomething(10); 如果不加explicit，可以隐式转换
     return 0;
 }
